78313d4aa387a0a475b7b0a501bde1442b9a7b5a
Gabriel Coelho <gabrielcoelhocn08@gmail.com>
2024-07-18 08:03:54 -0300

    created new react project and minor adjustments at starting project

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,4 +1,5 @@
 export default function TabButton({ children,isSelected, ...props}) { // Here ...props ,or wahtever name we choose, are creating an object and storing values in it, in this case isSelected and onClick.
+
   return (
     <li>
       <button className={isSelected ? 'active' : undefined} {...props}>
