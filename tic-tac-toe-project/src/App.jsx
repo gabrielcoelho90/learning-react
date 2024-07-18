c07d083e512238a537b59a29904e63d05feeac8c
Gabriel Coelho <gabrielcoelhocn08@gmail.com>
2024-07-18 14:47:40 -0300

    created Player component and feature that allow users change their names

--- tic-tac-toe-project/src/App.jsx
+++ tic-tac-toe-project/src/App.jsx
@@ -1,10 +1,19 @@
+import Player from "./components/Player";
+import { useState } from "react";
 
 function App() {
-  
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="player 1" symbol="X"></Player>
+          <Player initialName="player 2" symbol="O"></Player>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ tic-tac-toe-project/src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player({initialName, symbol}) {
+  const [playerName, setPlayerName] = useState(initialName)
+  const [isEditing, setIsEditing] = useState(false)
+
+  function handleEditClick(){
+    setIsEditing(editing => !editing) //explanation at class 78 -> updating a state based on a old state.
+  }
+
+  function changeName(event) {
+    setPlayerName(event.target.value)
+  }
+
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? <span className="player-name">{playerName}</span> : <input type="text" required value={playerName} onChange={changeName}></input>}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'save' : 'edit'}</button>
+  </li>
+  );
+}
